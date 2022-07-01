f253b115619c7f7a99fa04fcb0904598ec8dfc3c
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-01 14:38:12 +0700

    update marker on map

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # location-website
  just a simple project to practice typescript and google map api
+
+# ScreenShoot
+
+![img.png](images/img.png)
+![img_1.png](images/img_1.png)
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
-//
-// const user = new User();
-// const company = new Company();
+import { User } from "./User";
+import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const user = new User();
+const company = new Company();
+
+import { CustomMap } from "./CustomMap";
+
+const customMap = new CustomMap("map");
+
+customMap.addMarker(user);
+customMap.addMarker(company);
